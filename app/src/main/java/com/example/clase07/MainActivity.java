4748c41a8b1af52f31a598e6149d6b7ebbacaa6d
grecia17 <100071350@cientifica.edu.pe>
2022-10-12 19:35:41 -0500

    clase7

---
+++ app/src/main/java/com/example/clase07/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.clase07;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.bar:
+                Toast.makeText(this,"Soy Bar",Toast.LENGTH_LONG).show();
+            case R.id.refresh:
+                Toast.makeText(this,"Soy Refresh", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
